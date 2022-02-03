f6f7398a46efb535304616d6075d902a5085a5e1
LethalSainth <ikechukwuedwin01@gmail.com>
2022-02-03 19:04:04 +0100

    Add readme.md

--- py_regex_project.py
+++ py_regex_project.py
@@ -1,10 +1,3 @@
-'''This program attempts to match lines of input against a list of regular expressions for each line
-the name of the first matching pattern is printed, or unknown 
-if no mtching pattern is matched.
-
-when run without any arguments, line will be rea from standard input.
-If given a filename, lines will be read from that file.'''
-
 import re,sys
 
 def print_match(string, patterns):
