6e6eb5516fcbb5f5e6b05b5bc99f89be40c1b00d
LethalSainth <ikechukwuedwin01@gmail.com>
2022-03-29 00:43:30 +0100

    renamed 'py-regex-project'

--- pyregex_project.py
+++ pyregex_project.py
@@ -1,4 +1,11 @@
-import re,sys
+'''This program attempts to match lines of input against a list of regular expressions for each line
+the name of the first matching pattern is printed, or unknown 
+if no mtching pattern is matched.
+
+when run without any arguments, line will be read from standard input.
+If given a filename, lines will be read from that file.'''
+
+import re, sys
 
 def print_match(string, patterns):
     """
